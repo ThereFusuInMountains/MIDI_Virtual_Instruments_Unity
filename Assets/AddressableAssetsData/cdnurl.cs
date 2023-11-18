3d917b0b1ed25effbe7afd208f2af9e71cdd0565
陈君 <2548766690@qq.com>
2023-11-18 14:18:02 +0800

    [更新cdn相关]

--- Assets/AddressableAssetsData/cdnurl.cs
+++ Assets/AddressableAssetsData/cdnurl.cs
@@ -1,4 +1,1 @@
-class UOS
-{
-    public const string cdn_url = "https://a.unity.cn/client_api/v1/buckets/265482ae-0d88-4dd2-bbdb-3eec29294049/release_by_badge/latest/entry_by_path/content/?path=";
-}
+class UOS { public static string cdn_url = "https://a.unity.cn/client_api/v1/buckets/e0317ea4-afa8-42e0-aef1-c21c551e5be2/release_by_badge/latest/entry_by_path/content/?path="; }
